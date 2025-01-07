b29282ba134292ec992acdedbf270a4d112f180e
Manjri Verma <129585310+manjri555@users.noreply.github.com>
2025-01-07 12:26:56 +0530

    Add files via upload

---
+++ dockerwithsql-springapp/src/test/java/com/dockerwithsql/DockerwithsqlSpringappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dockerwithsql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerwithsqlSpringappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
