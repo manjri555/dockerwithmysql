c4a430fa222dae3ed7cc4719a6cd4ca70836fa10
Manjri Verma <129585310+manjri555@users.noreply.github.com>
2025-01-07 11:23:12 +0530

    Add files via upload

---
+++ dockerwithsql-springapp/src/main/java/com/dockerwithsql/DockerwithsqlSpringappApplication.java
@@ -0,0 +1,13 @@
+package com.dockerwithsql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerwithsqlSpringappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerwithsqlSpringappApplication.class, args);
+	}
+
+}
